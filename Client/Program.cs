3922b3bfc7518a9a42f560e94edb7a445ac47ef1
Supegg <supegg.rao@gmail.com>
2016-03-31 11:43:50 +0800

    test

---
+++ Client/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Client
+{
+    class Program
+    {
+        static volatile int errors = 0;
+        static volatile int tcpNum = 0;
+        static int robots = 1;
+        static int repeat = 100;
+        //static object l = new object();//for lock
+        static List<TcpClient> tcpList = new List<TcpClient>();
+        static Stopwatch watch = new Stopwatch();
+          
+        static void Main(string[] args)
+        {
+            presure();
+
+            do
+            {
+                Console.Write(DateTime.Now.ToLongTimeString());
+                Console.WriteLine("  Robots:{0}*{1}\t, tcpNums: {2}, errors:{3}\t, run times:{4}", robots, repeat, tcpNum, errors, watch.ElapsedMilliseconds);
+                Thread.Sleep(1000);
+            } while (true);
+
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+        private static void presure()
+        {
+            string server = "127.0.0.1";
+            int port = 9998;
+            List<Thread> ts = new List<Thread>();
+
+            Console.WriteLine("{0}\tTcp并发服务压力测试\r\n", DateTime.Now.ToLongTimeString());
+
+            for (int i = 0; i < robots; i++)
+            {
+                ts.Add(new Thread(new ParameterizedThreadStart((o) =>
+                {
+                    for (int r = 0; r < repeat; r++)
+                    {
+
+                        try
+                        {
+                            byte[] buf = Guid.NewGuid().ToByteArray();
+                            Array.Resize<byte>(ref buf, 1000);
+                            TcpClient conn = new TcpClient(server, port);
+                            tcpList.Add(conn);
+                            object[] state = new object[] {
+                                conn,
+                                buf
+                            };
+
+                            conn.GetStream().BeginWrite(buf, 0, buf.Length, new AsyncCallback(write), state);
+
+                            tcpNum++;
+
+                        }
+                        catch (Exception ex)
+                        {
+                            errors++;
+                            Console.WriteLine("{0}\t{1}\t{2}", Thread.CurrentThread.Name, errors, ex.Message);
+                        }
+                    }
+
+                })) { Name = "T" + i });
+            }
+
+            watch.Restart();
+            foreach (var t in ts)
+            {
+                t.Start();
+                Console.WriteLine(t.Name);
+            }
+
+            //foreach (var t in ts)
+            //{
+            //    t.Join();
+            //}
+        }
+
+
+        private static void read(IAsyncResult ar)
+        {
+            object[] state = (object[])ar.AsyncState;
+            TcpClient conn = (TcpClient)state[0];
+            byte[] buf = (byte[])state[1];
+
+            try
+            {
+                int bytesRead = conn.GetStream().EndRead(ar);
+                conn.GetStream().BeginWrite(buf, 0, bytesRead, new AsyncCallback(write), state);
+            }
+            catch (Exception ex)
+            {
+                errors++;
+                Console.WriteLine(ex.Message);
+                conn.GetStream().Close();
+                conn.Close();
+            }
+        }
+
+        private static void write(IAsyncResult ar)
+        {
+            object[] state = (object[])ar.AsyncState;
+            TcpClient conn = (TcpClient)state[0];
+            byte[] buf = (byte[])state[1];
+
+            try
+            {
+                conn.GetStream().EndWrite(ar);
+                conn.GetStream().BeginRead(buf, 0, buf.Length, new AsyncCallback(read), state);
+            }
+            catch (Exception ex)
+            {
+                errors++;
+                Console.WriteLine(ex.Message);
+                conn.GetStream().Close();
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        static List<TcpClient> tcpList = new List<TcpClient>();
+        static TcpListener listener = new TcpListener(IPAddress.Any, 9998);
+        static int rwCount = 0;
+        //static object l = new object();
+          
+        static void Main(string[] args)
+        {
+            start();
+              
+            do
+            {
+                Console.WriteLine("Clients: {0}, rwCount: {1}", tcpList.Count, rwCount);
+                rwCount = 0;
+                Thread.Sleep(1000);
+            } while (true);
+
+            Console.WriteLine("press any key to quit....");
+            Console.Read();
+            Environment.Exit(0);
+
+        }
+
+        static void start()
+        {
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(acceptCallback), listener);
+        }
+
+        private static void acceptCallback(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            try
+            {
+                TcpClient conn = listener.EndAcceptTcpClient(ar);
+                tcpList.Add(conn);
+                byte[] buf = new byte[1024];
+                object[] state = new object[] {
+                    conn,
+                    buf
+                };
+
+                conn.GetStream().BeginRead(buf, 0, buf.Length, new AsyncCallback(read), state);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                try
+                {
+                    listener.BeginAcceptTcpClient(
+                        new AsyncCallback(acceptCallback),
+                        listener);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void read(IAsyncResult ar)
+        {
+            object[] state = (object[])ar.AsyncState;
+            TcpClient conn = (TcpClient)state[0];
+            byte[] buf = (byte[])state[1];
+
+            try
+            {
+                int bytesRead = conn.GetStream().EndRead(ar);
+                rwCount++;
+
+                conn.GetStream().BeginWrite(buf, 0, bytesRead, new AsyncCallback(write), state);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                conn.GetStream().Close();
+                conn.Close();
+            }
+        }
+
+        private static void write(IAsyncResult ar)
+        {
+            object[] state = (object[])ar.AsyncState;
+            TcpClient conn = (TcpClient)state[0];
+            byte[] buf = (byte[])state[1];
+
+            try
+            {
+                conn.GetStream().EndWrite(ar);
+                rwCount++;
+                conn.GetStream().BeginRead(buf, 0, buf.Length, new AsyncCallback(read), state);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                conn.GetStream().Close();
+                conn.Close();
+            }
+        }
+    }
+}
